86e82aecf8148c40f0f96a452971a1defe12ada3
Grzegorz "lvndlord" Piluk <grze.piluk@gmail.com>
2026-07-02 16:20:17 +0200

    Update Next.js configuration with output settings and image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/https://github.com/lvvndlord/my-portfolio',
 };
 
 export default nextConfig;
